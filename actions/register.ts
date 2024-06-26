c96ee9d71ba1bf1fcd52bf30f0de3b9abd07b47f
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-06-26 14:25:19 +0200

    Register Functionality

--- actions/register.ts
+++ actions/register.ts
@@ -2,13 +2,33 @@
 
 import * as z from 'zod';
 import { RegisterSchema } from "@/schemas";
+import bcrypt from 'bcrypt';
+import { db } from  '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
-    if (!validatedFields) {
+    if (!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
-    
-    return { success: "Email sent!" };
+    const { email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return {error: "Email zajęty"}
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    // TODO : Send verifitaction token email
+
+    return { success: "Konto utworzone!" };
 }
