7032fd591bae76ea01f970f683f3555dbdac597c
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-08-01 23:42:16 +0200

    changeing login button on header

--- components/header/StartButton.tsx
+++ components/header/StartButton.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from "@/components/auth/login-button";
 
 const StartButton = () => {
     return ( 
-        <LoginButton mode="modal" asChild> 
+        <LoginButton mode="redirect" asChild> 
             <Button variant={`accent`}>
                 <span className="text-white font-mjolnir pt-1">
                     WEJDŹ
